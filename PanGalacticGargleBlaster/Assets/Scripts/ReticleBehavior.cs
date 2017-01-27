59f8365200f224efba0b52c5afc70d505f164255
Brendan H <bdharwood@live.com>
2017-01-26 23:08:57 -0700

    Adding a reticle that follows the cursor around the screen

---
+++ PanGalacticGargleBlaster/Assets/Scripts/ReticleBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticleBehavior : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.5f);
+        transform.position = Camera.main.ScreenToWorldPoint(pos);
+        transform.LookAt(new Vector3(0, 0, 0));
+    }
+}
