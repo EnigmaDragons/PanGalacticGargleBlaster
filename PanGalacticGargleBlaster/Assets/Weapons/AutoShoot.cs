8ba2e0f8808c6c08fe02b1d0e80477d6eeebaf57
Tim Reinagel <tmr2_handbell@yahoo.com>
2017-01-26 23:09:09 -0700

    AutoShooter + Bullet included.

---
+++ PanGalacticGargleBlaster/Assets/Weapons/AutoShoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoShoot : MonoBehaviour {
+
+    public Rigidbody Bullet;
+    //public float speed = 5f;
+    public double deltaTime;
+    public double timePerShot;
+    
+    void Start()
+    {
+        deltaTime = 0.0;
+    }
+    
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+
+        if (deltaTime > timePerShot)
+        {
+            deltaTime -= timePerShot;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        var bullet = (GameObject)Instantiate(Bullet, transform.position, Quaternion.identity);
+    }
+}
